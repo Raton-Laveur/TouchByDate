913187d8031c0a70d700519e8836693e89b517b8
Raton-Laveur <raton-laveur@raton-laveur.net>
2019-11-28 15:40:38 +0100

    Update TouchByDate.ps1

--- TouchByDate.ps1
+++ TouchByDate.ps1
@@ -1,8 +1,29 @@
 # TouchByDate
+# Des fichiers nommés par ordre alphabétique mais avec des timecodes mélangés ? Voici la solution ! 
+#
 # Touch : Set-ItemProperty -Path $filetotouch -Name LastWriteTime -Value (get-date)
 
+# erreur sur l'accent si UiCulture est autre que fr-fr : chercher une solution UTF8 pour read-host
+[Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+# est-ce que ça va le corriger ? 
+
+# Dossier à cibler : 
 $Dossier = Read-Host "Quel répertoire ?"
-Set-Location $Dossier
+if ($Dossier)
+{
+    Set-Location $Dossier
+}
+else 
+{
+    Throw "T'as rien écrit bouffon"
+    #quit
+}
+
+# Trier les fichiers par ordre alphabétique :
 $ListeFichiers = Get-ChildItem | Sort-Object -Property Name
-Set-ItemProperty $ListeFichiers -Name LastWriteTime -Value (Get-Date)
+
+# Remettre les timecodes à plat : 
+Get-ChildItem $ListeFichiers | ForEach-Object -Process {Set-ItemProperty $_ -Name LastWriteTime -Value (Get-Date)}
+
+# Afficher le résultat :
 Get-ChildItem
